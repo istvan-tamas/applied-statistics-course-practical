8ecc877af9d39dc109db3ae03f51b74975403028
istvan-tamas <istvan.tamas90@gmail.com>
2024-01-28 21:01:35 +0100

    update

--- exam.jl
+++ exam.jl
@@ -3,8 +3,7 @@ function solve(N,K)
 end
 
 function expected(p)
-    grade = rand(2:5)
-    return (grade * p) + (1.0 * (1-p))
+    return p*((1/4 * 2) + (1/4 * 3) + (1/4 * 4) + (1/4 * 5))
 end
 
 n,k=parse.(Int,split(readline()))
