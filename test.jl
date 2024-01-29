b01bd797082ddc504a10e0d81e1665f2c013f21a
istvan-tamas <istvan.tamas90@gmail.com>
2024-01-29 15:45:02 +0100

    Update

--- test.jl
+++
@@ -1,5 +0,0 @@
-function p_dice(dice, n)
-    return sum(1/6 * p_dice(dice-1, n-outcome) for outcome in 1:6)
-end
-
-println(p_dice(2, 7))
--- walk.jl
+++
@@ -1,8 +0,0 @@
-function randwalk(T,N,n)
-    dt = T/N
-    dW = sqrt(dt) .* randn(N,n)
-    cumsum!(dW,dW,dims=1)
-    return 0:dt:T-dt,dW
-end
-
-println(randwalk(1,2,2))
