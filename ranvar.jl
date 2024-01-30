aa699f233ac7b8f4ffd3060d910798f409cc2299
istvan-tamas <istvan.tamas90@gmail.com>
2024-01-30 10:47:10 +0100

    Update

---
+++ ranvar.jl
@@ -0,0 +1,28 @@
+ using Statistics
+ 
+ u = [-99, 40, 33, -85]
+ sig = [0.0625, 0.1875, 0.4375, 0.3125]
+
+exp = 0
+
+
+for i in 1:length(u)
+    global exp += sum(u[i]*sig[i])
+end
+
+sd = 0
+
+mean = sum(u)/length(u)
+
+for j in 1:length(sig)
+
+    #global sd = sum((u[j]-Statistics.mean(u))^2/length(u))
+    global sd = sqrt(sum((u[j]-exp)^2*sig[j]))
+end
+
+#ez yo
+sd2 = sqrt(sum((u[j]-exp)^2*sig[j] for j in 1:length(sig)))
+
+println(exp)
+println(sd)
+println(sd2)
