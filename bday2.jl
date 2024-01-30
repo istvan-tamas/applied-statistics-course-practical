48cee2b3eee61928fccea5667fce5bdafbc7b147
istvan-tamas <istvan.tamas90@gmail.com>
2024-01-30 17:50:37 +0100

    Update

--- bday2.jl
+++ bday2.jl
@@ -1,13 +1,9 @@
 function bday_p(p)
     x = 0.0
-    if p <= 0.0
-        return 1
-    else
-        for i in range(1,365)
-            x = 1 - prod(1.0 .- range(0,i-1)/365.0)
-            if x >= p
-                return i
-            end
+    for i in range(1,365)
+        x = 1 - prod(1.0 .- range(0,i-1)/365.0)
+        if x >= p
+            return i
         end
     end
 end
