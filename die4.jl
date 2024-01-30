fa8f1d17845eddc1083c079874784560c134caf6
istvan-tamas <istvan.tamas90@gmail.com>
2024-01-30 18:00:56 +0100

    Update

--- die4.jl
+++ die4.jl
@@ -1,7 +1,17 @@
+function max_die(n,m)
+    k=(M^n)-((M-1)^n)
+    P=k/(6^n)
+    return P
+end
+
 n,M=parse.(Int,split(readline()))
 
-k=(M^n)-((M-1)^n)
-P=k/(6^n)
-
-
-println(P)
+if (n >= 30 || n < 0)
+    return 0
+else
+    if (M > 10 || M <= 0)
+        return 0
+    else
+        println(max_die(n,M))
+    end
+end
